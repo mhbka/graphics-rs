1baf8027ff29e1fd481105cea2d96628144f0651
M Hisham <hisham.khairul@sg.wilmar-intl.com>
2024-03-04 18:22:31 +0800

    hello window section completed :D

--- opengl/src/main.rs
+++ opengl/src/main.rs
@@ -1,24 +1,41 @@
-use glfw::{Action, Context, Key};
+use glfw::{Action, Context, Key, OpenGlProfileHint, WindowHint};
 use glfw::fail_on_errors;
 use gl::types::*;
 
+
+
 fn main() {
-    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+    // init glfw and set window hints as found in tutorial
+    let mut glfw = glfw::init(fail_on_errors!()).unwrap();    
+    glfw.window_hint(WindowHint::ContextVersionMajor(3));
+    glfw.window_hint(WindowHint::ContextVersionMinor(3));
+    glfw.window_hint(WindowHint::OpenGlProfile(OpenGlProfileHint::Core));
 
     // Create a windowed mode window and its OpenGL context
-    let (mut window, events) = glfw.create_window(300, 300, "Hello this is window", glfw::WindowMode::Windowed)
+    let (width, height) = (640, 480);
+    let (mut window, events) = glfw.create_window(width, height, "Hello", glfw::WindowMode::Windowed)
         .expect("Failed to create GLFW window.");
+
+    // Load window's function pointers into `gl`
+    gl::load_with(|s| {window.get_proc_address(s)});
 
     // Make the window's context current
     window.make_current();
     window.set_key_polling(true);
 
-    gl::load_with(|s| window.get_proc_address(s));
+    // Set viewport + framebuffer size callback fn
+    // SELF-NOTE: the closure is supposed to take a `Window` param, but since it's already tied to the `gl` instance, its not necessary
+    window.set_framebuffer_size_callback(|_, w, h| unsafe {gl::Viewport(0, 0, w, h)});
 
     // Loop until the user closes the window
     while !window.should_close() {
         // Swap front and back buffers
         window.swap_buffers();
+
+        unsafe { 
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT); 
+        };
 
         // Poll for and process events
         glfw.poll_events();
