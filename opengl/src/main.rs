893c200e903638425ddef1992a51c30264e370a0
M Hisham <hisham.khairul@sg.wilmar-intl.com>
2024-03-05 13:54:24 +0800

    workin on sum opengl

--- opengl/src/main.rs
+++ opengl/src/main.rs
@@ -1,6 +1,10 @@
+mod shader;
+
 use glfw::{Action, Context, Key, OpenGlProfileHint, WindowHint};
 use glfw::fail_on_errors;
 use gl::types::*;
+use std::mem::size_of_val;
+use std::ptr::null;
 
 
 
@@ -27,14 +31,43 @@ fn main() {
     // SELF-NOTE: the closure is supposed to take a `Window` param, but since it's already tied to the `gl` instance, its not necessary
     window.set_framebuffer_size_callback(|_, w, h| unsafe {gl::Viewport(0, 0, w, h)});
 
+    // Set shaders
+    unsafe {
+        shader::create_and_link_shaders();
+    }
+
     // Loop until the user closes the window
     while !window.should_close() {
         // Swap front and back buffers
         window.swap_buffers();
 
+       // Setting buffer and passing in vertex data
         unsafe { 
+            // Set bg color
             gl::ClearColor(0.2, 0.3, 0.3, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT); 
+
+            // Generate a VBO, then bind it to array buffer
+            let mut vbo: u32 = 0;
+            gl::GenBuffers(1, &mut vbo as *mut u32);
+            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+
+            // Pass vertex data into the buffer
+            let data = [
+                -0.5, -0.5, 0.0,
+                0.5, -0.5, 0.0,
+                0.0, 0.5, 0.0
+            ];
+            gl::BufferData(
+                gl::ARRAY_BUFFER, 
+                size_of_val(&data).try_into().unwrap(), 
+                data.as_ptr() as *const GLvoid, // cast to pointer, then pointer type to GLvoid/c_void
+                gl::STATIC_DRAW
+            );
+
+            // Specify how to read array buffer data
+            gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 3*64, null());
+            gl::EnableVertexAttribArray(0);
         };
 
         // Poll for and process events
---
+++ opengl/src/shader.rs
@@ -0,0 +1,78 @@
+use std::ffi::{CStr, CString};
+use std::ptr::{null, null_mut};
+
+use gl::types;
+
+const VERTEX_SHADER_SOURCE: &str = "
+    #version 330 core
+    layout (location = 0) in vec3 aPos;
+    void main()
+    {
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    }
+";
+
+const FRAGMENT_SHADER_SOURCE: &str = "
+    #version 330 core
+    out vec4 FragColor;
+
+    void main()
+    {
+        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    } 
+";
+
+
+// main shader entrypoint
+// compiles shaders and links them to shader program object
+pub unsafe fn create_and_link_shaders() -> u32 {
+    let shader_program = gl::CreateProgram();
+    let vertex_shader = compile_shader(VERTEX_SHADER_SOURCE, gl::VERTEX_SHADER);
+    let fragment_shader = compile_shader(FRAGMENT_SHADER_SOURCE, gl::FRAGMENT_SHADER);
+
+    gl::AttachShader(shader_program, vertex_shader);
+    gl::AttachShader(shader_program, fragment_shader);
+    gl::LinkProgram(shader_program);
+    
+    gl::DeleteShader(vertex_shader);
+    gl::DeleteShader(fragment_shader);
+
+    shader_program
+}
+
+
+// compiles a shader, given its source code and a shader type
+// if it fails to compile, info will be printed out but the shader will still be returned
+unsafe fn compile_shader(shader_source: &str, shader_type: types::GLenum) -> u32 {
+    let c_str = CString::new(shader_source)
+        .unwrap()
+        .as_ptr(); // should be ok, once the shader compiles i dont need this anymore
+
+    let c_str_ptr: *const *const i8 = &c_str;
+    let shader = gl::CreateShader(shader_type);
+    gl::ShaderSource(shader, 1, c_str_ptr, null());
+    gl::CompileShader(shader);
+
+    check_compilation_status(shader);
+
+    shader
+}
+
+
+// checks status of compilation
+// if failed, write info to a CString then print it out
+unsafe fn check_compilation_status(shader: u32) -> bool {
+    let mut success = 0;
+    gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut success as *mut i32);   
+
+    if success <= 0 {
+        let mut info_vec = vec![0; 512];
+        gl::GetShaderInfoLog(shader, 512,null_mut(), info_vec.as_mut_ptr());
+        let info_log = CStr::from_ptr(info_vec.as_mut_ptr()).to_str().unwrap();
+        println!("error: vertex shader compilation failed \n {info_log}");
+        false
+    } else {
+        println!("vertex shader compilation succeeded");
+        true
+    }
+}
