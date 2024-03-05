894e1d3a05a2cb6fb8336a848995a722382ed17d
M Hisham <hisham.khairul@sg.wilmar-intl.com>
2024-03-05 18:05:03 +0800

    debugging EBO

---
+++ opengl/src/init.rs
@@ -0,0 +1,37 @@
+use gl::types::GLvoid;
+use std::mem::{size_of, size_of_val};
+use std::ptr::null;
+
+/** Performs initialization for openGL. */
+pub unsafe fn gl_init(vertices: &[f32], indices: &[usize], vao: &mut u32, vbo: &mut u32, ebo: &mut u32) {
+    // bound check
+    if indices.len()%3 != 0 {panic!("indices array length is not modulo 3.")}
+
+    // generate and bind VBO, then copy vertex data into array buffer
+    gl::GenBuffers(1, vbo as *mut u32);
+    gl::BindBuffer(gl::ARRAY_BUFFER, *vbo);
+    gl::BufferData(
+        gl::ARRAY_BUFFER, 
+        size_of_val(&vertices).try_into().unwrap(), 
+        vertices.as_ptr() as *const GLvoid,
+        gl::STATIC_DRAW
+    );
+
+    // generate and bind EBO, then copy index data into element buffer
+    gl::GenBuffers(1, ebo as *mut u32);
+    gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, *ebo);
+    gl::BufferData(
+        gl::ELEMENT_ARRAY_BUFFER,
+        size_of_val(&indices).try_into().unwrap(),
+        indices.as_ptr() as *const GLvoid,
+        gl::STATIC_DRAW
+    );
+
+    // generate and bind VAO
+    gl::GenVertexArrays(1, vao as *mut u32);
+    gl::BindVertexArray(*vao);
+
+    // set vertex attribute pointers for VAO
+    gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 3 * size_of::<f32>() as i32, null());
+    gl::EnableVertexAttribArray(0);
+}
--- opengl/src/main.rs
+++ opengl/src/main.rs
@@ -1,11 +1,11 @@
+mod init;
 mod shader;
 
 use glfw::{Action, Context, Key, OpenGlProfileHint, WindowHint};
 use glfw::fail_on_errors;
 use gl::types::*;
-use std::mem::{size_of, size_of_val};
-use std::ptr::null;
 use std::env;
+use std::ptr::null;
 
 
 fn main() {
@@ -13,11 +13,9 @@ fn main() {
 
     // init glfw and set window hints as found in tutorial
     let mut glfw = glfw::init(fail_on_errors!()).unwrap();    
-    /* 
     glfw.window_hint(WindowHint::ContextVersionMajor(3));
     glfw.window_hint(WindowHint::ContextVersionMinor(3));
     glfw.window_hint(WindowHint::OpenGlProfile(OpenGlProfileHint::Core));
-    */
 
     // Create a windowed mode window and its OpenGL context
     let (width, height) = (640, 480);
@@ -31,31 +29,23 @@ fn main() {
     window.make_current();
     window.set_key_polling(true);
 
-    // Initialize and bind a VAO and its VBO
-    let data: [f32; 9] = [
-        -0.5, -0.5, 0.0,
-        0.5, -0.5, 0.0,
-        0.0, 0.5, 0.0
+    // our vertex and index data
+    let vertices: Vec<f32> = vec![
+        0.5,  0.5, 0.0,  // top right
+        0.5, -0.5, 0.0,  // bottom right
+       -0.5, -0.5, 0.0,  // bottom left
+       -0.5,  0.5, 0.0   // top left 
     ];
+    let indices: Vec<usize> = vec![
+        0, 1, 3,
+        1, 2, 3
+    ];
+
+    // Initialize and bind VAO, VBO, EBO
     let mut vao: u32 = 0;
     let mut vbo: u32 = 0;
-    unsafe {
-        gl::GenBuffers(1, &mut vbo as *mut u32);
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl::BufferData(
-            gl::ARRAY_BUFFER, 
-            size_of_val(&data).try_into().unwrap(), 
-            data.as_ptr() as *const GLvoid,
-            gl::STATIC_DRAW
-        );
-
-        gl::GenVertexArrays(1, &mut vao as *mut u32);
-        gl::BindVertexArray(vao);
-
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 3 * size_of::<f32>() as i32, null());
-        gl::EnableVertexAttribArray(0);
-    }
+    let mut ebo: u32 = 0;
+    unsafe { init::gl_init(vertices.as_slice(), indices.as_slice(), &mut vao, &mut vbo, &mut ebo) };
 
     let shader_program = unsafe { shader::create_and_link_shaders() };
     
@@ -75,10 +65,10 @@ fn main() {
             // Use shader + bind VAO
             gl::UseProgram(shader_program);
             gl::BindVertexArray(vao);
-            //gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, i%50, null());
-            
+        
             // Draw triangles
-            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+            gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, null());
         };
 
         
